67c5d040f8a0add692559b9ed92c95a86e5ea889
Benoit Lord <benoit.lord@canada.ca>
2020-07-30 12:58:00 -0400

    First commit from TFS cs4345

---
+++ settings/settings-general.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+define({
+
+
+	/*------------------ Course info -----------------*/
+	courseLegacyCode:"K099",
+	seriesTitle_en: "K099 Cultural self-reflection:",
+	seriesTitle_fr: "K099 Introspection culturelle : ",
+	courseTitle_en: "What I know and what I don’t know",
+	courseTitle_fr: "ce que je sais et ce que j’ignore",
+
+	showGlossary: true,
+	triggerCompletionWhenAllPagesViewed: true,
+
+	"googleAnalyticsID": "UA-101907595-19",
+
+	addToolboxPage:[
+		//example
+		//local page loaded through ajax (remember to add the lightbox code (copy paste from Glossary))
+		// { "name_en":"Custom popup", "name_fr":"popup \"custom\"", "filename_en":"custom_en", "filename_fr":"custom_fr"},
+		//PDF
+		// { "name_en":"Print Version(PDF)", "name_fr":"Version imprimable(PDF)", "filename_en":"custom_en.pdf", "filename_fr":"print_version_fr.pdf"}
+
+	],
+
+    editMode: true
+
+});
